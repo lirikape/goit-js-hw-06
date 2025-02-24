12d69fd6912b6063647716dfe90a307c6ea8f929
lirikape <lirikape@gmail.com>
2025-02-24 11:18:43 +0200

    тест ланцюга

--- js/task-01.js
+++ js/task-01.js
@@ -19,3 +19,18 @@ categoryItems.forEach(item => {
   console.log(`Category: ${categoryTitle}`);
   console.log(`Elements: ${categoryItemsCount}`);
 });
+
+const students = [
+  { name: "Манго", score: 83, courses: ["математика", "фізика"] },
+  { name: "Полі", score: 59, courses: ["інформатика", "математика"] },
+  { name: "Аякс", score: 37, courses: ["фізика", "біологія"] },
+  { name: "Ківі", score: 94, courses: ["література", "інформатика"] },
+];
+
+
+const uniqueSortedCourses = students
+  .flatMap(student => student.courses)
+  .filter((course, index, array) => array.indexOf(course) === index)
+  .sort((a, b) => a.localeCompare(b));
+
+console.log(uniqueSortedCourses); // ['біологія', 'інформатика', 'література', 'математика', 'фізика']
